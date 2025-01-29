ffccf9e968fe6258889bb2d3403ffebf0de32f55
Sikander-Mirza <sikandersunny2017@gmail.com>
2025-01-28 18:55:58 -0800

    behance link

--- Frontend/components/portfolio/OurWork.jsx
+++ Frontend/components/portfolio/OurWork.jsx
@@ -1,18 +1,14 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import portfolio from "../../src/assets/Portfolio-02.png";
 import "./OurWork.css"; // Import your CSS file
 
 const OurWork = () => {
-  const navigate = useNavigate(); // Hook for navigation
-
   const handleImageClick = () => {
-    navigate('/portfolio'); // Navigate to the portfolio route
+    window.open('https://www.behance.net/graphicblok1319', '_blank'); // Open in a new tab
   };
 
   return (
-    <div className="our-work" style={{marginBottom:"2rem"}}>
-      <h1>Our Work</h1>
+    <div className="our-work" style={{ marginBottom: "2rem" }}>
       <img 
         src={portfolio} 
         alt="Portfolio"
